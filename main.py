5d4db17cb1389867d8c72cd68acebacca439b45f
sihamshm <48486345+sihamshm@users.noreply.github.com>
2024-11-27 17:21:26 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+import sys
+
+module_path='model'
+
+
+if module_path not in sys.path: 
+    sys.path.append(module_path)
+    
+import ml_models
+from ml_models import*
+
+
+
+
+if __name__ == "__main__":
+    data_icu = "ICU"
+    cv= 0 # int(5) int(10) cross-validation 5 or 10 fold validation
+    concat = 'Conactenate'
+    oversampling = 'True'
+    model = 'Random Forest'  #'Logistic Regression','Random Forest','Gradient Bossting','Xgboost'
+    ml=ml_models.ML_models(data_icu,cv,model,concat,oversampling)
